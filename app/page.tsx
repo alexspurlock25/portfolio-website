9cbcc5af140909583fc5a2929b5971e25724aa26
Alexander Spurlock <alexanderspurlock@gmail.com>
2024-05-05 11:27:13 -0400

    added all content from previous website here

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
 export default function Page() {
   return (
     <section>
-      <h1 className="font-medium text-2xl mb-8 tracking-tighter">my portfolio</h1>
+      {/* <h1 className="font-medium text-2xl mb-8 tracking-tighter">my portfolio</h1> */}
       <img className="mt-5 w-60 rounded-full mx-auto shadow-lg" src="./avatar.jpeg" />
       <div className="m-auto w-11/12 mt-10 md:w-5/12 tracking-wider">
         <p className="prose prose-neutral dark:prose-invert">
-          Full time Data Engineer and BS in Software Engineering grad. I love making iOS apps and Android apps to bring my ideas to life.
+          Hi! Welcome to my website! I am a Data Engineer with a Bachelor's degree in Software Engineering. I am very passionate about crafting innovative iOS and Android applications to bring ideas to life.
         </p>
       </div>
     </section>
