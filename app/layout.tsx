a53bc04e3b933a45d2a7b2591b3d2f5744e7b5e8
Alexander Spurlock <alexanderspurlock@gmail.com>
2024-05-06 16:36:05 -0400

    fix home page align

--- app/layout.tsx
+++ app/layout.tsx
@@ -54,8 +54,8 @@ export default function RootLayout({
       <head>
         <SandpackCSS />
       </head>
-      <body className="antialiased max-w-2xl mb-40 flex flex-col md:flex-row mx-4 mt-8 lg:mx-auto">
-        <main className="flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0">
+      <body className="antialiased mx-auto max-w-2xl mb-40 md:flex-row mt-8 lg:mx-auto">
+        <main className="min-w-0 mt-6 px-2 md:px-0">
           <Navbar />
           {children}
           <Analytics />
--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Page() {
       <div className="md:flex items-center">
         <img className="w-60 rounded-full mx-auto shadow-lg" src="./avatar.jpeg" />
         <div className="mt-10 md:mt-0 ml-0 md:ml-10">
-          <p className="prose prose-neutral dark:prose-invert">
+          <p className="prose prose-neutral dark:prose-invert text-center md:text-left">
             Hi! Welcome to my website! I am a Data Engineer with a Bachelor's degree in Software Engineering. I am very passionate about crafting innovative iOS and Android applications to bring ideas to life.
           </p>
         </div>
